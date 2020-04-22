6429493f4df05a89e6059aff3debc1432984232d
Xicheng.Gong <xicheng.gong@accenture.com>
2020-04-22 15:06:54 +0800

    update

--- src/DesignPatternDemo/01SingletonPattern/Program.cs
+++ src/DesignPatternDemo/01SingletonPattern/Program.cs
@@ -9,4 +9,42 @@ namespace _01SingletonPattern
             Console.WriteLine("Hello World!");
         }
     }
+
+
+    public sealed class Singleton1
+    {
+        private static readonly Singleton1 instance = new Singleton1();
+
+        // 显式静态构造函数告诉C＃编译器
+        // 不要将类型标记为BeforeFieldInit
+        // 如果没有静态类的应用可以去掉这个构造函数
+        static Singleton1()
+        {
+        }
+
+        private Singleton1()
+        {
+        }
+
+        public static Singleton1 Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+
+    public sealed class Singleton
+    {
+        private static readonly Lazy<Singleton> lazy = new Lazy<Singleton>(() => new Singleton());
+
+        public static Singleton Instance { get { return lazy.Value; } }
+
+        private Singleton()
+        {
+        }
+    }
+
+
 }
